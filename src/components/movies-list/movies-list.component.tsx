09afbe1912745dfe99372e61d528924bfe6dcef8
Timofey_Yatsenko <timofey_yatsenko@epam.com>
2017-09-30 20:51:10 +0200

    movie list header implementation

--- src/components/movies-list/movies-list.component.tsx
+++ src/components/movies-list/movies-list.component.tsx
@@ -1,38 +1,64 @@
 import React, { Component } from 'react';
 import styles from './movies-list.scss';
 import { MoviesItemComponent } from './movie-item/movie-item.component';
-import { MovieItemModel } from './movie-item/movie-item.model';
-import { MoviesListHeaderComponent } from './movie-list-header/movies-list-header.component';
-import {movies} from './movies-db';
+import { movies } from './movies-db';
+import { RadioGroupComponent, RadioGroupOption } from '../common/radio-group/radio-group.component';
 
-interface IMoviesListComponentState {
-  items: MovieItemModel[];
-}
+export class MoviesListComponent extends Component {
+  private sortingOptions: RadioGroupOption[] = [
+    { value: 'releaseDate', name: 'release date' },
+    { value: 'rating', name: 'rating' },
+  ];
 
-export class MoviesListComponent extends Component<any, IMoviesListComponentState> {
-  public state: IMoviesListComponentState = {
+  public state = {
     items: movies,
+    sortingOptions: this.sortingOptions,
+    sorting: this.sortingOptions[0],
   };
 
-  public render() {
+  private handleSortingChange(sorting: RadioGroupOption) {
+    this.setState({sorting});
+  }
+
+  private renderMovies() {
     return (
       <div className={styles.host}>
-        <MoviesListHeaderComponent/>
-
-        {this.state.items.length && (
-          <div className={styles.list}>
-            {this.state.items.map((item) => (
-              <MoviesItemComponent item={ item } key={ item.id } />
-            ))}
+        <div className={styles.header}>
+          <span className={styles.total}>
+            {this.state.items.length} movies found
+          </span>
+          <div className={styles.sorting}>
+            <span>Sort by</span>
+            <RadioGroupComponent
+              options={this.state.sortingOptions}
+              selected={this.state.sorting}
+              onSelect={this.handleSortingChange.bind(this)}
+            />
           </div>
-        )}
-
-        {this.state.items.length === 0 && (
-          <div className={styles.emptyState}>
-            Nothing found
-          </div>
-        )}
+        </div>
+        <div className={styles.list}>
+          {this.state.items.map((item) => (
+            <MoviesItemComponent item={ item } key={ item.id } />
+          ))}
+        </div>
       </div>
     );
+  }
+
+  private renderEmptyState() {
+    return (
+      <div className={styles.host}>
+        <div className={styles.header}/>
+        <div className={styles.emptyState}>
+          No films found
+        </div>
+      </div>
+    );
+  }
+
+  public render() {
+    return this.state.items.length
+      ? this.renderMovies()
+      : this.renderEmptyState();
   }
 }
