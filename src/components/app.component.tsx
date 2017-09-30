b2e3e2b6658854f2c8e43a15f25fa6c2f148124a
Timofey_Yatsenko <timofey_yatsenko@epam.com>
2017-09-30 22:22:57 +0200

    install react router

--- src/components/app.component.tsx
+++ src/components/app.component.tsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { SearchComponent } from './search/search.component';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route } from 'react-router';
 
 export class App extends Component {
   public render() {
     return (
-      <SearchComponent/>
+      <Router>
+        <Route path='/' component={SearchComponent}/>
+      </Router>
     );
   }
 }
