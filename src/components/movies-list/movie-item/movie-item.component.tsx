7d21acbdb216022c1e5defce58c92bd8ad1978a8
Timofey_Yatsenko <timofey_yatsenko@epam.com>
2017-09-30 21:00:25 +0200

    rest of elements for movie-item

--- src/components/movies-list/movie-item/movie-item.component.tsx
+++ src/components/movies-list/movie-item/movie-item.component.tsx
@@ -13,6 +13,10 @@ export class MoviesItemComponent extends Component<MoviesItemComponentProps> {
         <img className={styles.poster} src={this.props.item.posterUrl}/>
         <div className={styles.meta}>
           <span className={styles.movieName}>{this.props.item.name}</span>
+          <span className={styles.year}>{this.props.item.releaseYear}</span>
+        </div>
+        <div className={styles.meta}>
+          <span className={styles.kind}>{this.props.item.kind}</span>
         </div>
       </div>
     );
