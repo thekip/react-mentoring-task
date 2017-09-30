147ca76ca910a9af2c4fada27b529b0ee74a9e98
Timofey_Yatsenko <timofey_yatsenko@epam.com>
2017-09-30 19:11:46 +0200

    radio-group functionality

--- src/components/common/radio-group/radio-group.component.tsx
+++ src/components/common/radio-group/radio-group.component.tsx
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import styles from './radio-group.scss';
 
-export class RadioGroupComponent extends Component {
+export interface RadioGroupOption {
+  value: string;
+  name: string;
+}
+
+export interface RadioGroupProps {
+  options: RadioGroupOption[];
+  type?: string;
+  selected: RadioGroupOption;
+  onSelect: (option: RadioGroupOption) => void;
+}
+
+export class RadioGroupComponent extends Component<RadioGroupProps> {
+  private isSelected(option: RadioGroupOption) {
+    return this.props.selected.value === option.value;
+  }
+
   public render() {
     return (
       <div className={styles.host}>
-        <span className={styles.title}>Search by</span>
-        <div className={styles.item}>Title</div>
-        <div className={styles.item}>Director</div>
+        {this.props.options.map((item) => (
+          <button key={item.value} type='button'
+            onClick={() => this.props.onSelect(item)}
+            className={this.isSelected(item) ? styles.itemActive : styles.item }>
+            {item.name}
+            </button>
+        ))}
       </div>
     );
   }
--- src/components/header/search/search.component.tsx
+++ src/components/header/search/search.component.tsx
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import styles from './search.scss';
-import { RadioGroupComponent } from '../../common/radio-group/radio-group.component';
+import { RadioGroupComponent, RadioGroupOption } from '../../common/radio-group/radio-group.component';
 
 export class SearchComponent extends Component {
+  private searchByOptions: RadioGroupOption[] = [
+    { value: 'title', name: 'Title' },
+    { value: 'director', name: 'Director' },
+  ];
+
+  public state = {
+    searchByOptions: this.searchByOptions,
+    searchBy: this.searchByOptions[0],
+  };
+
+  private handleSelectSearchBy(searchBy: RadioGroupOption) {
+    this.setState({searchBy});
+  }
+
   public render() {
     return (
       <div className={styles.host}>
@@ -10,7 +24,12 @@ export class SearchComponent extends Component {
         <form>
           <input type='text' placeholder='Kill Bill 2' name='search' className={styles.input}/>
           <div className={styles.actionsRow}>
-            <RadioGroupComponent/>
+            <span>Search by</span>
+            <RadioGroupComponent
+              selected={this.state.searchBy}
+              options={this.state.searchByOptions}
+              onSelect={this.handleSelectSearchBy.bind(this)}
+            />
             <button className={styles.submitBtn} type='submit'>Search</button>
           </div>
         </form>
